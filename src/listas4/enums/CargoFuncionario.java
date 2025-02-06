b483a9a55390531f9d0bc87c3c12b972f73a5a0b
Ricardo Rodrigues Santana <140956804+Ric-ardo28@users.noreply.github.com>
2025-02-05 22:31:04 -0300

    Apliquei enum em projetos que eu ja tinha , para praticar

--- src/listas4/enums/CargoFuncionario.java
+++ src/listas4/enums/CargoFuncionario.java
@@ -1,17 +1,28 @@
 package listas4.enums;
 
 public enum CargoFuncionario {
-	GERENTE("Gerente Geral"),
-	SUPERVISOR("Supervisor de Patio"),
-	CONFERENTE("Conferente de Carga"),
-	AJUDANTE("Ajudante de Armazem");
+	GERENTE("Gerente Geral", "Gerente", 9000.00),
+	SUPERVISOR("Supervisor de Patio", "Supervisor", 7000.00),
+	CONFERENTE("Conferente de Carga","Conferente", 5000.00),
+	AJUDANTE("Ajudante de Armazem", "Ajudante", 3000.00);
 
 	private String descricao;
+	private String nome;
+	private double salario;
 
-	CargoFuncionario(String descricao) {
+	CargoFuncionario(String descricao, String nome, double salario) {
 		this.descricao = descricao;
+		this.nome = nome;
+		this.salario = salario;
 	}
 	public String getDescricao() {
 		return descricao;
+	}
+	public String getNome(){
+		return nome;
+	}
+
+	public double getSalario() {
+		return salario;
 	}
 }
