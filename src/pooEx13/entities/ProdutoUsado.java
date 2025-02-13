279f7e95aba90628d79d53679eb24854e6fa89e9
Ricardo Rodrigues Santana <140956804+Ric-ardo28@users.noreply.github.com>
2025-02-12 23:46:53 -0300

    exercicios de fixação Herança e polimorfismo

--- src/pooEx13/entities/ProdutoUsado.java
+++ src/pooEx13/entities/ProdutoUsado.java
@@ -1,26 +1,28 @@
 package pooEx13.entities;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ProdutoUsado extends Produto {
-	private LocalDateTime dataFaturamento;
+	private LocalDate dataFaturamento;
 
-	public ProdutoUsado(String nome, Double preco, LocalDateTime dataFaturamento) {
+	public ProdutoUsado(String nome, Double preco, LocalDate dataFaturamento) {
 		super(nome, preco);
 		this.dataFaturamento = dataFaturamento;
 	}
 
-	public LocalDateTime getDataFaturamento() {
+	public LocalDate getDataFaturamento() {
 		return dataFaturamento;
 	}
 
-	public void setDataFaturamento(LocalDateTime dataFaturamento) {
+	public void setDataFaturamento(LocalDate dataFaturamento) {
 		this.dataFaturamento = dataFaturamento;
 	}
 	@Override
 	public String tagPreco() {
 		return getNome() +
 				" (usado) $ " + String.format("%.2f", getPreco()) +
-				"(Data de faturamento: " + getDataFaturamento() + ")";
+				"(Data de faturamento: " + dataFaturamento.format(DateTimeFormatter.ofPattern("dd/mm/yyyy")) + ")";
 	}
 }
